77c91a984cb9afe2b352560143911c076a7af2ea
sridharv <sridhar@laddoo.net>
2016-03-01 11:21:18 +0100

    Add timedout + godoc

--- fail.go
+++ fail.go
@@ -1,12 +1,46 @@
-// Package fail simplifies error handling in tests using panic+recover
+// Package fail simplifies error handling using panic+recover
+//
+// It is primarily intended for use in tests. Sample usage is below:
+//
+// func TestSomething(t *testing.T) {
+// 	// fail.Using recovers from panics and calls t.Fatal with a stack trace if any errors occur.
+// 	defer fail.Using(t.Fatal)
+//
+// 	file, err := os.Open("myfile")
+// 	// Panic with a failure if err is non-nil. The panic is recovered by the call to fail.Using
+// 	// and t.Fatal is automatically called.
+// 	fail.IfErr(err)
+//
+// 	defer fail.IfDeferred(file.Close, "failed to close myfile")
+//
+//   	data, err := ioutil.ReadAll(file)
+// 	fail.IfErr(err, "failed to read contents of myfile")
+// 	expected := "expected data"
+// 	fail.If(string(data) != expected, string(data), " != ", expected)
+// }
 package fail
 
 import (
 	"reflect"
 	"runtime/debug"
 	"strings"
+	"time"
 )
 
+func clean(str string) string {
+	bracket, lineIndex := strings.LastIndex(str, "("), strings.LastIndex(str, " +0x")
+	switch {
+	case strings.HasSuffix(str, ")") && bracket != -1:
+		return str[0:bracket]
+	case lineIndex != -1:
+		return str[0:lineIndex]
+	default:
+		return str
+	}
+}
+
+// Using recovers from panics and calls failure with the result of the recovery. It must be used as part of a deferred
+// call.
 func Using(failer func(...interface{})) {
 	r := recover()
 
@@ -26,7 +60,7 @@ func Using(failer func(...interface{})) {
 				include = true
 			}
 			if include {
-				squashed = append(squashed, part)
+				squashed = append(squashed, clean(part))
 			}
 		}
 		failer(append(f, strings.Join(squashed, "\n"))...)
@@ -41,20 +75,55 @@ type namer struct{}
 
 var pkgPath = reflect.TypeOf(namer{}).PkgPath()
 
-func IfErr(err error, args...interface{}) {
+// TimedOut returns true if the function passed in takes longer than
+// timeout to run.
+func TimedOut(fn func(), timeout time.Duration) bool {
+	ch := make(chan struct{})
+	go func() {
+		fn()
+		close(ch)
+	}()
+	select {
+	case <-ch:
+		return false
+	case <-time.After(timeout):
+		return true
+	}
+}
+
+// IfErr panics if err is non-nil, constructing a failure message with the error and args.
+// It must be used in conjunction with Using.
+func IfErr(err error, args ...interface{}) {
 	if err != nil {
 		panic(failure(append([]interface{}{err}, args...)))
 	}
 }
 
-func IfDeferred(fn func() error, args...interface{}) {
+// IfDeferred panics if the error returned by fn is non-nil, constructing a failure message with the error and args.
+// It must be used in conjunction with Using, to check for errors in deferred functions. Sample usage is below:
+//
+// func TestSomething(t *testing.T) {
+// 	defer fail.Using(t.Fatal)
+// 	file, err := os.Open("myfile")
+//  	fail.IfErr(err)
+// 	defer fail.IfDeferred(file.Close, "closing myfile failed")
+// 	...
+// }
+func IfDeferred(fn func() error, args ...interface{}) {
 	if err := fn(); err != nil {
 		panic(failure(append([]interface{}{err}, args...)))
 	}
 }
 
-func IfNot(condition bool, args ...interface{}) {
-	if !condition {
+// If panics if the condition is true, constructing a failure message with the arguments passed in.
+// It must be used in conjuction with Using.
+func If(condition bool, args ...interface{}) {
+	if condition {
 		panic(failure(args))
 	}
+}
+
+// Message returns a failure message that can be recovered by a call to Using.
+func Message(args ...interface{}) interface{} {
+	return failure(args)
 }
